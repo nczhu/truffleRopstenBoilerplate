bf0d8998b35135a67cf29f23e977925d3c7949b3
Nicole Zhu <nicolezhuis@gmail.com>
2018-08-24 11:28:13 +0700

    Fix: fixed 2_deploy_contracts.js script to not take input params

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 var HelloWorld = artifacts.require("HelloWorld");
 
 module.exports = function(deployer) {
-  deployer.deploy(HelloWorld, "hello");
+  deployer.deploy(HelloWorld);
   // Additional contracts can be deployed here
+  // e.g. deployer.deploy(Contract, input_1, input...);
 };
